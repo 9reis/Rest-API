5b2ca46452affaf708fd4990d99087c9a479814a
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-16 19:56:40 -0300

    Add o metodo GET one

--- rest-api/src/main/java/com/lucasreis/restapi/controller/ClienteController.java
+++ rest-api/src/main/java/com/lucasreis/restapi/controller/ClienteController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +17,20 @@ import com.lucasreis.restapi.model.Cliente;
 import com.lucasreis.restapi.respositories.ClienteRepository;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping
+	@GetMapping("/clientes")
 	public List <Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/cliente/{id}")
+	public Cliente cliente(@PathVariable Long id) {
+		return clienteRepository.findById(id).get();
 	}
 	
 	@PostMapping
@@ -31,4 +38,5 @@ public class ClienteController {
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+	
 }
