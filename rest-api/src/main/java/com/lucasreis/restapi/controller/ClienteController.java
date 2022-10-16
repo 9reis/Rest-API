21ce6689ba1fe5ad2f842dac6e33fa841bb1188c
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-16 17:16:45 -0300

    Finalizado o Verbo http GET

--- rest-api/src/main/java/com/lucasreis/restapi/controller/ClienteController.java
+++ rest-api/src/main/java/com/lucasreis/restapi/controller/ClienteController.java
@@ -2,18 +2,23 @@ package com.lucasreis.restapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasreis.restapi.model.Cliente;
+import com.lucasreis.restapi.respositories.ClienteRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping
 	public List <Cliente> listar() {
-		
+		return clienteRepository.findAll();
 	}
 }
